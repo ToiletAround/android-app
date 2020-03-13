6c3ffc699d59fb2ba231f9dcaeed8dbbf51db018
Rúben Silva <rubensilva945@gmail.com>
2020-03-13 17:27:15 +0000

    fix(project): fixed project

--- app/src/main/java/dev/rubensilva/toiletaround/MapsActivity.kt
+++ app/src/main/java/dev/rubensilva/toiletaround/MapsActivity.kt
@@ -19,7 +19,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         setContentView(R.layout.activity_maps)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
+                .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
@@ -36,7 +36,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(34.0, 151.0)
+        val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
